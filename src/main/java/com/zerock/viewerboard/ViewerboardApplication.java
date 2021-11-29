6df0953a07d63bf95d21a8f529683d272fcc8811
Kjisu <gabrieljisu9@gmail.com>
2021-11-29 14:41:34 +0900

    initialCommit

---
+++ src/main/java/com/zerock/viewerboard/ViewerboardApplication.java
@@ -0,0 +1,13 @@
+package com.zerock.viewerboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewerboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewerboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerock/viewerboard/ViewerboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerock.viewerboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewerboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
